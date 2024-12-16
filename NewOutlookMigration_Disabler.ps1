5017cbb15cf8ffd75f44461ebe4defccb436d685
LemmyFl <123549154+LemmyFl@users.noreply.github.com>
2024-12-16 10:56:17 +0100

    Update NewOutlookMigration_Disabler.ps1

--- NewOutlookMigration_Disabler.ps1
+++ NewOutlookMigration_Disabler.ps1
@@ -1,16 +1,19 @@
 $users = Get-ChildItem "Registry::HKEY_USERS" | Where-Object { $_.Name -match 'S-\d-\d+(-\d+)+$' }
 
-$officeVersions = @("14.0", "16.0")
-
 foreach ($user in $users) {
-    foreach ($version in $officeVersions) {
-        $path = "Registry::" + $user.Name + "\Software\Policies\Microsoft\office\$version\outlook\preferences"
+    $officeBasePath = "Registry::" + $user.Name + "\Software\Policies\Microsoft\Office"
+    if (Test-Path -Path $officeBasePath) {
+        $officeVersions = Get-ChildItem -Path $officeBasePath | Where-Object { $_.Name -match '\d+\.\d+' }
 
-        if (-not (Test-Path -Path $path)) {
-            New-Item -Path $path -Force | Out-Null
+        foreach ($version in $officeVersions) {
+            $path = $version.PSPath + "\outlook\preferences"
+            
+            if (-not (Test-Path -Path $path)) {
+                New-Item -Path $path -Force
+            }
+            Set-ItemProperty -Path $path -Name "NewOutlookMigrationUserSetting" -Value 0 -Type DWord
+            Set-ItemProperty -Path $path -Name "NewOutlookAutoMigrationRetryIntervals" -Value 0 -Type DWord
+            Set-ItemProperty -Path $path -Name "DoNewOutlookAutoMigration" -Value 0 -Type DWord
         }
-        Set-ItemProperty -Path $path -Name "NewOutlookMigrationUserSetting" -Value 0 -Type DWord
-        Set-ItemProperty -Path $path -Name "NewOutlookAutoMigrationRetryIntervals" -Value 0 -Type DWord
-        Set-ItemProperty -Path $path -Name "DoNewOutlookAutoMigration" -Value 0 -Type DWord
     }
 }
