25b35703aa58e24167bf94ee6e0b146186b5b53a
LemmyFl <123549154+LemmyFl@users.noreply.github.com>
2024-12-16 15:23:07 +0100

    Update NewOutlookMigration_DisablerV2.ps1

--- NewOutlookMigration_DisablerV2.ps1
+++ NewOutlookMigration_DisablerV2.ps1
@@ -1,5 +1,4 @@
 $TaskName = "DisableNewOutlookMigration"
-
 $ActionScript = @"
 $RegPath = 'HKCU:\Software\Policies\Microsoft\Office\16.0\Outlook\Preferences'
 $Keys = @{
@@ -7,32 +6,22 @@ $Keys = @{
     'NewOutlookAutoMigrationRetryIntervals' = 0
     'DoNewOutlookAutoMigration' = 0
 }
-
-# Registry-Schlüssel erstellen/ändern
-New-Item -Path $RegPath -Force | Out-Null
+New-Item -Path $RegPath -Force
 foreach ($Key in $Keys.Keys) {
     Set-ItemProperty -Path $RegPath -Name $Key -Value $Keys[$Key]
 }
-Write-Host '[INFO] Registry-Werte für Outlook-Migration erfolgreich gesetzt.'
 "@
-
 $ScriptPath = "C:\kworking\DisableNewOutlookMigration.ps1"
 
 if (-not (Test-Path "C:\kworking")) {
-    New-Item -ItemType Directory -Path "C:\kworking" | Out-Null
-    Write-Host "[INFO] Ordner 'C:\kworking' wurde erstellt."
+    New-Item -ItemType Directory -Path "C:\kworking"
 }
-
 Set-Content -Path $ScriptPath -Value $ActionScript -Encoding UTF8
-Write-Host "[INFO] Ausführbares Skript unter '$ScriptPath' erstellt."
 
-Write-Host "[INFO] Erstelle Scheduled Task '$TaskName'..."
 schtasks.exe /create `
     /tn $TaskName `
     /tr "powershell.exe -ExecutionPolicy Bypass -File $ScriptPath" `
     /sc onlogon `
     /ru "%username%" `
     /rl HIGHEST `
-    /f | Out-Null
-
-Write-Host "[SUCCESS] Task '$TaskName' wurde erstellt. Das Skript wird bei jedem Benutzer-Login ausgeführt."
+    /f
