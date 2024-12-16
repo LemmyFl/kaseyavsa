fdd97a7201368a6897c6ede413cd20ff45ea61da
LemmyFl <123549154+LemmyFl@users.noreply.github.com>
2024-12-16 10:18:11 +0100

    Update NewOutlookMigration_Disabler.ps1

--- NewOutlookMigration_Disabler.ps1
+++ NewOutlookMigration_Disabler.ps1
@@ -5,4 +5,8 @@ foreach ($user in $users) {
         New-Item -Path $path -Force | Out-Null
     }
     Set-ItemProperty -Path $path -Name "NewOutlookMigrationUserSetting" -Value 0 -Type DWord
+    Set-ItemProperty -Path $path -Name "NewOutlookAutoMigrationRetryIntervals" -Value 0 -Type DWord
+    Set-ItemProperty -Path $path -Name "DoNewOutlookAutoMigration" -Value 0 -Type DWord
+    Set-ItemProperty -Path $path -Name "DisableOutlookPSTCreation" -Value 1 -Type DWord  # Neue Zeile 1
+    Set-ItemProperty -Path $path -Name "DisableCrossAccountAutoDiscover" -Value 1 -Type DWord  # Neue Zeile 2
 }
