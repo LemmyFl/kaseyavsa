92e7d9f529f13d56765c42b44aced66e71ac93ba
LemmyFl <123549154+LemmyFl@users.noreply.github.com>
2024-12-17 07:39:02 +0100

    Update NewOutlookMigration_DisablerV2.ps1

--- Outlook/NewOutlookMigration_DisablerV2.ps1
+++ Outlook/NewOutlookMigration_DisablerV2.ps1
@@ -1,1 +1,11 @@
-test
+<#
+.NOTES
+  Version:        01.02.00
+  Author:         <LemmyFL>
+  Last Change Date:  17.12.2024
+#>
+# Start-Process -FilePath powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command & { $(iwr 'https://raw.githubusercontent.com/LemmyFl/WindowsScripts/main/WindowsRepairScript.ps1' -UseBasicParsing).Content }" -Verb RunAs
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
