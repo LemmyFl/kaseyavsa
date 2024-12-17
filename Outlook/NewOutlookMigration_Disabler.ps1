cf49b31ad54434d2129f202451b8761255312c76
LemmyFl <123549154+LemmyFl@users.noreply.github.com>
2024-12-17 10:00:57 +0100

    Update NewOutlookMigration_Disabler.ps1

--- Outlook/NewOutlookMigration_Disabler.ps1
+++ Outlook/NewOutlookMigration_Disabler.ps1
@@ -13,29 +13,35 @@ $ScriptPath = "C:\kworking\SetOutlookRegistryKeys.ps1"
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
-function Save-RegistryScript {
+function Save-DisableOutlookMigrationScript {
     $ActionScript = @"
-`$RegPath = "HKCU:\SOFTWARE\Policies\Microsoft\Office\16.0\Outlook\Preferences"
+`$RegSubPath = "Software\Policies\Microsoft\Office\16.0\Outlook\Preferences"
 `$RegistryValues = @{
     'NewOutlookMigrationUserSetting'        = 0
     'NewOutlookAutoMigrationRetryIntervals' = 0
     'DoNewOutlookAutoMigration'             = 0
 }
-New-Item -Path `$RegPath -Force | Out-Null
-`$RegistryValues.GetEnumerator() | ForEach-Object {
-    New-ItemProperty -Path `$RegPath -Name `$_.Key -PropertyType DWord -Value `$_.Value -Force | Out-Null
+
+`$UserSIDs = Get-WmiObject -Class Win32_UserProfile | Where-Object { 
+    `$\_.Loaded -eq `$true -and `$\_.Special -eq `$false 
+} | Select-Object -ExpandProperty SID
+
+foreach (`$SID in `$UserSIDs) {
+    `$RegPath = "Registry::HKEY_USERS\`$SID\`$RegSubPath"
+
+    New-Item -Path `$RegPath -Force | Out-Null
+
+    foreach (`$Key in `$RegistryValues.Keys) {
+        Set-ItemProperty -Path `$RegPath -Name `$Key -Value `$RegistryValues[`$Key] -Type DWord -Force
+    }
 }
 "@
-    New-Item -ItemType Directory -Path (Split-Path -Path $ScriptPath -Parent) -Force -ErrorAction SilentlyContinue | Out-Null
-    Set-Content -Path $ScriptPath -Value $ActionScript -Encoding UTF8 -Force
-}
 
-function Register-RunOnceTask {
-    Start-Process -FilePath "schtasks.exe" -ArgumentList @(
-        "/create", "/tn `"$TaskName`"", 
-        "/tr `"powershell.exe -ExecutionPolicy Bypass -File `"$ScriptPath`"`"", 
-        "/sc onlogon", "/rl HIGHEST", "/f", "/z", "/it"
-    ) -NoNewWindow -Wait
+    # Ensure the directory exists
+    New-Item -ItemType Directory -Path (Split-Path -Path $ScriptPath -Parent) -Force -ErrorAction SilentlyContinue | Out-Null
+
+    # Save the script content to the specified path
+    Set-Content -Path $ScriptPath -Value $ActionScript -Encoding UTF8 -Force
 }
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
